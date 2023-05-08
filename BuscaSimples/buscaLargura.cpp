6e065709b589b9c7f2a2a5dbe3f8304cb102d741
Arthur <80659617+artfrc@users.noreply.github.com>
2023-05-08 12:18:48 -0300

    Commit
    More commit

--- BuscaSimples/buscaLargura.cpp
+++ BuscaSimples/buscaLargura.cpp
@@ -5,18 +5,27 @@
 using namespace std;
 
 typedef pair<int,int> pii;
-typedef pair<pii,int> ppi;
-typedef pair<pii,pii> ppp;
 typedef vector<vector<int>> vvi;
 typedef queue<vector<vector<int>>> qvvi;
 
-int dx[] = {-1,1,0,0};
+// auxilia para percorrer a matriz
+int dx[] = {-1,1,0,0}; 
 int dy[] = {0,0,-1,1};
+
+
 int OPC; // 1 -> Jogo quebra-cabeça ; 2 -> Jogo do labirinto
 int totalNodes = 0;
-int flag = 0; // Irá ajudar marcar a posição que irei inserir meu novo estado no vetor de matrizes
-pii BEGIN, END;
-set<vvi> checkedStates;
+
+pii BEGIN; // posição que personagem começa no labirinto ou vazio quebra-cabeça
+
+pii END; // posição final para o personagem no labirinto
+
+set<vvi> checkedStates; // guardo os estados já visitados
+
+/* =================================================================
+/   DEIXEI ESSAS VARIÁVEIS GLOBAIS PARA FACILITAR COM AS FUNÇÕES!
+/   FICARIAM MUITOS PARÂMETROS PARA PASSAR
+/ ================================================================= */ 
 
 bool isSolution(int i, int j, vvi mtx, int N) {
     // Verifica se cheguei na solução do problema
@@ -30,7 +39,6 @@ bool isSolution(int i, int j, vvi mtx, int N) {
             for(int i = 0; i < N and ok; i++) {
                 for(int j = 0; j < N and ok; j++) {
                     if(i == N/2 and j == N/2) continue;
-                    // cout << "count = " << count << " mtx[i][j] = " << mtx[i][j] << endl; 
                     if(mtx[i][j] != count) {
                         ok = false;
                     }
@@ -47,20 +55,22 @@ bool isSolution(int i, int j, vvi mtx, int N) {
 bool solve(int i, int j, qvvi& qMatrix, int N) {
     // Aqui será feito a busca em largura para os problemas
     queue<pii> Queue;
-    // Guardo a posição i e j que estou e também guardo o index (idxArr) do vetor de Matrizes que devo guardar meu próximo estado
-    Queue.push({i,j});
-    int state = 0;
+    Queue.push({i,j}); // Guardo a posição i e j
+    
     while(!Queue.empty()) {
         vvi matrix = qMatrix.front(); // matriz representando o estado atual
         qMatrix.pop();
 
-        // posições que estou na matriz atual
+        // posições que personagem está no labirinto ou espaço vazio no quebra-cabeça
         i = Queue.front().first;
         j = Queue.front().second;
         Queue.pop();
 
-        // cout << ">> Posicao: [" << i << "][" << j << "]" << endl;
-        cout << ">> Estado: " << state++ << endl;
+
+        // Printa o estado que estou
+        
+        cout << ">> Estado: " << totalNodes++ << endl;
+        
         for(int l = 0; l < N; l++) {
             for(int r = 0; r < N; r++) {
                 cout << matrix[l][r] << " ";
@@ -68,16 +78,28 @@ bool solve(int i, int j, qvvi& qMatrix, int N) {
             cout << endl;
         }
         cout << endl;
+        
+        // =====================================================
 
-        totalNodes++;
+        // Verifica se cheguei na solução
         if(isSolution(i,j,matrix,N)) {
             return true;
         }
+
+        // Faço a busca pelos possíveis próximos estados
         for(int k = 0; k < 4; k++) {
             int x = i+dx[k];
             int y = j+dy[k];
+            
+            // Verifico se são posições válidas para a matriz
             if(x < 0 or x >= N or y < 0 or y >= N) continue;
-            vvi newState(N,vector<int>(N));
+
+            // Se for labirinto, então não deixo visitar posições que são 0 na matriz
+            if(OPC == 2 and !matrix[x][y]) continue;
+
+            vvi newState(N,vector<int>(N)); // um possível estado que ainda não foi visitado
+            
+            // monto minha matriz/estado
             for(int l = 0; l < N; l++) {
                 for(int r = 0; r < N; r++) {
                     if(l == x and r == y) {
@@ -89,40 +111,34 @@ bool solve(int i, int j, qvvi& qMatrix, int N) {
                     }
                 }
             }
+
+            // verifico se já visitei esse estado
             if(checkedStates.find(newState) != checkedStates.end()) continue;
-            if(OPC == 2 and !matrix[x][y]) continue;
-            // cout << "x = " << x << " y = " << y  << " N = " << N << endl;
-            
-            // Guardo o meu novo estado
-            checkedStates.insert(newState);
-            qMatrix.push(newState);
-            Queue.push({x,y});
+
+            checkedStates.insert(newState); // registro que esse novo estado foi visitado
+            qMatrix.push(newState); // coloco ele na fila da busca
+            Queue.push({x,y}); // posições do meu novo estado que se encontra o personagem no labirinto ou posição vazia no quebra-cabeça
         }
     }
-    return false;
+    return false; // não encontrou solução
 }
 
 int main() { _
     int N; // N = matriz NxN
-    bool flag = false;
-    int t = 0;
     while(cin >> OPC >> N) {
         totalNodes = 0;
-        if(flag) cout << "==================================================" << endl;
-        flag = true;
-        cout << ">> Teste: " << ++t << endl;
         if(OPC == 1) {
             cout << "*** QUEBRA CABEÇA ***" << endl;
         } else {
             cout << "*** LABIRINTO ***" << endl;
         }
 
-        vvi matrix(N,vector<int>(N));
-        qvvi qMatrix;
+        vvi matrix(N,vector<int>(N)); // estado inicial
+        qvvi qMatrix; // fila de estados para a busca
 
         for(int i = 0; i < N; i++) {
             for(int j = 0; j < N; j++) {
-                // Lendo a matriz inicial e criando matriz de visitados
+                // Lendo a matriz inicial
                 cin >> matrix[i][j];
                 if(OPC == 1) { // Quebra-cabeça
                     if(matrix[i][j] == 0) { // Posição vaga no quebra-cabça
@@ -140,7 +156,9 @@ int main() { _
                 }
             }
         }
-        qMatrix.push(matrix); // Inserindo minha matriz inicial
+       
+        qMatrix.push(matrix); // Inserindo minha matriz inicial na fila
+       
         if(solve(BEGIN.first,BEGIN.second,qMatrix,N)) {
             cout << ">> Encontrei!" << endl;
         } else {
